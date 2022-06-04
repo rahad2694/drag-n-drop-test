64010b47e7bf76d806fa33845b747785986a9188
rahad2694 <rahad.haw@gmail.com>
2022-06-05 01:41:15 +0600

    Functional target achieved

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let draggedItem = null;
 
 for (const item of list_items) {
     item.addEventListener('dragstart', function () {
-        console.log('drag-Start');
+        // console.log('drag-Start');
         draggedItem = item;
         setTimeout(function () {
             item.style.display = 'none';
@@ -13,7 +13,7 @@ for (const item of list_items) {
     });
 
     item.addEventListener('dragend', function () {
-        console.log('drag-End');
+        // console.log('drag-End');
         setTimeout(function () {
             draggedItem.style.display = 'block';
             draggedItem = null;
@@ -31,13 +31,18 @@ for (const item of list_items) {
         });
 
         list.addEventListener('dragleave', function (e) {
-            // e.preventDefault();
             this.style.backgroundColor = 'rgba(0,0,0,0.1)';
         });
 
         list.addEventListener('drop', function (e) {
-            // e.preventDefault();
-            this.append(draggedItem);
+            // console.log(draggedItem.innerText);
+            let column = this.children;
+
+            if (draggedItem.innerText != column[0]?.innerText && draggedItem.innerText != column[1]?.innerText && draggedItem.innerText != column[2]?.innerText && draggedItem.innerText != column[3]?.innerText && draggedItem.innerText != column[4]?.innerText && draggedItem.innerText != column[5]?.innerText) {
+                this.append(draggedItem);
+            } else {
+                console.log('Already Exists!')
+            }
             this.style.backgroundColor = 'rgba(0,0,0,0.1)';
         });
     }
